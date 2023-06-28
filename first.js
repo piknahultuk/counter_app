232c88ac38a9cf1c3aa3b52778e723e9c4b5252f
kuutlhannnkp <52879315+kuutlhannnkp@users.noreply.github.com>
2023-06-28 08:55:40 +0300

    Checking for color when the number is negative, zero or pozitive

--- first.js
+++ first.js
@@ -7,13 +7,26 @@ resetnumber.addEventListener("click", reset);
 decreasenumber.addEventListener("click", decrease);
 increasenumber.addEventListener("click", increase);
 
+function colorChecker() {
+  if (parseInt(number.innerHTML) < 0) {
+    number.style.color = "red";
+  } else if (parseInt(number.innerHTML) > 0) {
+    number.style.color = "green";
+  } else {
+    number.style.color = "black";
+  }
+}
+
 function reset() {
   number.innerHTML = 0;
+  colorChecker();
 }
 
 function decrease() {
   number.innerHTML = parseInt(number.innerHTML) - 1;
+  colorChecker();
 }
 function increase() {
   number.innerHTML = parseInt(number.innerHTML) + 1;
+  colorChecker();
 }
